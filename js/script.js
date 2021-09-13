e1544af32701cc945f4d6c704b8285cf45c2084f
Alex Sandro <alex.sandro.1199@gmail.com>
2021-09-13 15:10:10 -0300

    refatoracao com class do scroll anima

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initDropDownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
 import initFuncionamento from './modules/funcionamento.js'
 import fetchAnimais from './modules/fetch-animais.js'
-import initAnimacaoScroll from './modules/scroll-animacao.js'
+import ScrollAnima from './modules/scroll-anima.js'
 import fetchBitcoin from './modules/fetch-bitcoin.js'
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
@@ -25,11 +25,13 @@ modal.init()
 const tooltip = new ToolTip('[data-tooltip]')
 tooltip.init()
 
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]')
+scrollAnima.init()
+
 initDropDownMenu()
 initMenuMobile()
 initFuncionamento()
 fetchAnimais('../../animaisapi.json', '.numeros-grid')
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco')
-initAnimacaoScroll()
 
 // ativar webpack npm run dev
